93fe885c50bf51334ed62711caf54b1d26e0e293
Huy Tran Lam <tlhuy02@gmail.com>
2025-08-14 23:56:19 +0700

    Initial commit

---
+++ app/Models/Task.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Schema;
+
+class Task extends Model
+{
+    //
+
+    protected $fillable = ['title', 'completed'];
+
+}
